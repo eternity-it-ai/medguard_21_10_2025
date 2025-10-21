02c93022040c95ff1f0834753113fb8cbad4a888
YotamOfri <yotamofri.b@gmail.com>
2025-10-21 12:06:47 +0300

    asdsad

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app.include_router(router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://medguard-21-10-2025.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
